0aa79f7307e21190b3ee20a843b912332755d8b7
Peter Key <63628694+peterekey@users.noreply.github.com>
2024-01-13 18:50:26 +1100

    Steps 1-3: Build Field class, including .print() method

--- main.js
+++ main.js
@@ -6,5 +6,30 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-    
+    constructor(arr) {
+        this._field = arr
+    }
+
+    get field() {
+        return this._field
+    }
+
+    get print() {
+        let output = ''
+        for (let i = 0; i < myField.field.length; i++) {
+            output += myField.field[i].join('')
+            if (i != myField.field.length-1) {
+                output += '\n'
+            }
+        }
+        return output
+    }
 }
+
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+
+  console.log(myField.print)
