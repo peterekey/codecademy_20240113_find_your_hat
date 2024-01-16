bd98193a9ae27bb109834c643a149b1660257037
Peter Key <63628694+peterekey@users.noreply.github.com>
2024-01-17 07:51:53 +1100

    Add check of path to hat

--- main.js
+++ main.js
@@ -36,7 +36,15 @@ class Field {
             finalField.push(generatedRow)
         }
 
+        console.log('finalField is:')
+        console.log(finalField)
+        console.log('finalField[0] is:')
+        console.log(finalField[0])
+        console.log('finalField[0][0] is:')
+        console.log(finalField[0][0])
         finalField[0][0] = '*'
+        console.log('finalField[0][0] is now:')
+        console.log(finalField[0][0])
 
         // Set hat position
         let hatRow
@@ -60,20 +68,55 @@ class Field {
                 holeRow == null && holeCol == null || 
                 holeRow === 0 && holeCol === 0 || 
                 holeRow === hatRow && holeCol === hatCol || 
-                holes.some(holeCoords => holeCoords.every((value, index) => value === [holeRow, holeCol][index]))
+                holes.some(holeCoords => holeCoords.every((value, index) => value === [holeRow, holeCol][index])) 
             ) {
                 holeRow = Math.floor(Math.random() * rows)
                 holeCol = Math.floor(Math.random() * cols)
             }
             holes.push([holeRow, holeCol])
         }
-        console.log('Holes are now at ', holes)
         for (const holeCoordinate of holes) {
-            
             finalField[holeCoordinate[0]][holeCoordinate[1]] = 'O'
         }
 
+        if (!this.isPathToHat(finalField)) {
+            this.generateField(rows, cols, holesPercentage)
+        }
+
         return finalField
+    }
+
+    isPathToHat(field) {
+        // Starting position
+        let queue = [[0,0]]
+
+        // Track visited tiles
+        let visited = new Set(['0,0'])
+
+        // While there are still tiles to visit
+        while (queue.length > 0) {
+            // Remove the first tile in the queue
+            let [row, col] = queue.shift()
+            // Check for the hat
+            if (field[row][col] === '^') {
+                return true
+            }
+            // if no hat, explore neighbours
+            const neighbours = [[row + 1, col], [row - 1, col], [row, col + 1], [row, col - 1]]
+            neighbours.forEach(([newRow, newCol]) => {
+                if (newRow >= 0 && 
+                    newRow < field.length && 
+                    newCol >= 0 && 
+                    newCol < field[row].length && 
+                    field[row][col] !== 'O' &&
+                    !visited.has(`${newRow},${newCol}`)
+                ) {
+                    queue.push([newRow, newCol])
+                    visited.add(`${newRow},${newCol}`)
+                }
+            })
+        }
+        return false //if no path found
     }
 
     getPosition(character) {
@@ -229,7 +272,7 @@ const handleInput = userInput => {
 }
 
 
-let myField = new Field(20, 20, 0.30)
+let myField = new Field(20, 20, 0.40)
 console.clear()
 process.stdout.write(myField.print + '\n' + 'Which way? ')
 process.stdin.on('data', handleInput)
